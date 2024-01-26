f032448af55b8be95b6e78e6168c3cef4c7d391f
DFLBB <dalape@msn.com>
2024-01-26 16:43:47 +0100

    Merge samartin

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,49 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlanzas- <dlanzas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:05:25 by dlanzas           #+#    #+#             */
-/*   Updated: 2023/08/14 14:16:21 by dlanzas-         ###   ########.fr       */
+/*   Updated: 2023/12/27 15:42:29 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long	ft_number(const char *str, int neg)
-{
-	long int	num;
-
-	num = 0;
-	while (*str > 47 && *str < 58)
-	{
-		num = (num * 10) + (*str - 48);
-		if (num > 2147483648 && neg == -1)
-			(ft_putstr_fd("Error\n", 2), exit(-1));
-		if (num > 2147483647 && neg == 1)
-			(ft_putstr_fd("Error\n", 2), exit(-1));
-		str++;
-	}
-	return (num);
-}
-
 int	ft_atoi(const char *str)
 {
-	size_t	num;
-	int		neg;
+	long	nb;
+	int		sign;
 
-	neg = 1;
-	while ((*str > 8 && *str < 14) || *str == 32)
+	nb = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
 		str++;
-	if (*str == 45 || *str == 43)
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
 	{
-		if (*str == 45)
-			neg *= -1;
+		nb = (nb * 10) + *str - '0';
 		str++;
 	}
-	if (*str > 47 && *str < 58)
-		num = ft_number(str, neg);
+	nb = nb * sign;
+	if (nb > __INT_MAX__)
+		return (-1);
+	else if (nb < (-(__INT_MAX__) - 1))
+		return (0);
 	else
-		(ft_putstr_fd("Error\n", 2), exit(-1));
-	return (num * neg);
+		return ((int)nb);
 }
